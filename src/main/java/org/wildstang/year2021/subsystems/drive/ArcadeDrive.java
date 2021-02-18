07bc10a7126a7643e232eeebd6b68323fcf41ce1
jwaters2461 <>
2021-02-17 19:31:31 -0600

    added safe zone for joystick of 0.1 from the middle

--- src/main/java/org/wildstang/year2021/subsystems/drive/ArcadeDrive.java
+++ src/main/java/org/wildstang/year2021/subsystems/drive/ArcadeDrive.java
@@ -60,8 +60,12 @@ public class ArcadeDrive implements Subsystem {
     // respond to input updates
     public void inputUpdate(Input signal) {
         if (signal == joystickLeftX || signal == joystickLeftY) {
+            if(Math.abs(joystickLeftY.getValue())>0.1&&Math.abs(joystickLeftX.getValue())>0.1){
             leftSpeed = (joystickLeftY.getValue() + joystickLeftX.getValue()) * speedMult;
-            rightSpeed = (joystickLeftY.getValue() - joystickLeftX.getValue()) * speedMult;
+            rightSpeed = (joystickLeftY.getValue() - joystickLeftX.getValue()) * speedMult;}
+        }
+        else {
+            resetState();
         }
     }
 

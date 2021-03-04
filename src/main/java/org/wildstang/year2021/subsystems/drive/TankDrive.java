6f1a1fe076e9ded5c2405ce26a0d5cbd7b3277df
WhiteHoodHacker <TheWhiteHoodHacker@gmail.com>
2021-03-03 20:00:23 -0600

    Added dead zone to joysticks

--- src/main/java/org/wildstang/year2021/subsystems/drive/TankDrive.java
+++ src/main/java/org/wildstang/year2021/subsystems/drive/TankDrive.java
@@ -4,7 +4,6 @@ import org.wildstang.year2021.robot.CANConstants;
 import org.wildstang.year2021.robot.WSInputs;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import org.wildstang.framework.core.Core;
@@ -68,10 +67,14 @@ public class TankDrive implements Subsystem {
     public void inputUpdate(Input signal) {
         // check to see which input was updated
         if (signal == leftJoystick) {
-            leftSpeed = leftJoystick.getValue() * multiplier;
+            if (leftJoystick.getValue() > 0.5) {
+                leftSpeed = leftJoystick.getValue() * multiplier;
+            }
         }
         if (signal == rightJoystick) {
-            rightSpeed = rightJoystick.getValue() * multiplier;
+            if (rightJoystick.getValue() > 0.5) {
+                rightSpeed = rightJoystick.getValue() * multiplier;
+            }
         }
     }
 

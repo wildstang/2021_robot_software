ef8d953fef5af4371e4166a19d5e5d97c480d3a3
smccrorie <42500981+smccrorie@users.noreply.github.com>
2021-04-06 19:54:16 -0500

    Search Paths!

--- src/main/java/org/wildstang/year2021/auto/programs/SearchAPath.java
+++ src/main/java/org/wildstang/year2021/auto/programs/SearchAPath.java
@@ -2,6 +2,7 @@ package org.wildstang.year2021.auto.programs;
 
 import org.wildstang.framework.auto.AutoProgram;
 import org.wildstang.framework.auto.steps.AutoParallelStepGroup;
+import org.wildstang.year2019.auto.steps.DelayStep;
 import org.wildstang.year2021.auto.steps.PathFollowerStep;
 import org.wildstang.year2021.auto.steps.intakeOnStep;
 
@@ -10,10 +11,11 @@ import frc.paths.SearchA;
 public class SearchAPath extends AutoProgram{
     @Override
     protected void defineSteps() {
-        AutoParallelStepGroup steps = new AutoParallelStepGroup();
-        steps.addStep(new PathFollowerStep(new SearchA().getPath()));
-        steps.addStep(new intakeOnStep());
-        addStep(steps);
+        //AutoParallelStepGroup steps = new AutoParallelStepGroup();
+        addStep(new intakeOnStep());
+        addStep(new DelayStep(0.15));
+        addStep(new PathFollowerStep(new SearchA().getPath()));
+        //addStep(steps);
     }
 
     @Override
--- src/main/java/org/wildstang/year2021/auto/programs/SearchBPath.java
+++ src/main/java/org/wildstang/year2021/auto/programs/SearchBPath.java
@@ -2,6 +2,7 @@ package org.wildstang.year2021.auto.programs;
 
 import org.wildstang.framework.auto.AutoProgram;
 import org.wildstang.framework.auto.steps.AutoParallelStepGroup;
+import org.wildstang.year2019.auto.steps.DelayStep;
 import org.wildstang.year2021.auto.steps.PathFollowerStep;
 import org.wildstang.year2021.auto.steps.intakeOnStep;
 
@@ -10,10 +11,11 @@ import frc.paths.SearchB;
 public class SearchBPath extends AutoProgram{
     @Override
     protected void defineSteps() {
-        AutoParallelStepGroup steps = new AutoParallelStepGroup();
-        steps.addStep(new PathFollowerStep(new SearchB().getPath()));
-        steps.addStep(new intakeOnStep());
-        addStep(steps);
+        //AutoParallelStepGroup steps = new AutoParallelStepGroup();
+        addStep(new intakeOnStep());
+        addStep(new DelayStep(0.1));
+        addStep(new PathFollowerStep(new SearchB().getPath()));
+        //addStep(steps);
     }
 
     @Override

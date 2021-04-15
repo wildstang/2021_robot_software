7acd94151832d9ea663a74feb480a4a424863188
nkocela2222 <45176718+nkocela2222@users.noreply.github.com>
2021-04-14 19:46:06 -0500

    Create ArmDownwardsStep.java

---
+++ src/main/java/org/wildstang/year2021/auto/steps/ArmDownwardsStep.java
@@ -0,0 +1,33 @@
+package org.wildstang.year2021.auto.steps;
+
+import org.wildstang.framework.auto.steps.AutoStep;
+import org.wildstang.year2021.subsystems.arm.Arm;
+
+public class ArmDownwardsStep extends AutoStep {
+    Arm arm = new Arm();
+    double speed = 0;
+
+    public ArmDownwardsStep() {
+        this(-1.0);
+    }
+
+    public ArmDownwardsStep(double: s) {
+        if (speed >= -1 && speed <= 1) {
+            speed = -Math.abs(s);
+        } else {
+            speed = 0;
+        }
+    }
+
+    public void initialize() {
+        arm.setArmSpeed(speed);
+    }
+
+    public void update() {
+        setFinished(true);
+    }
+
+    public String toString() {
+        return "ArmDownwardsStep";
+    }
+}

0f18521f7a85fe37827170507fcee17402ca02d5
dfruzyna2137 <43018879+dfruzyna2137@users.noreply.github.com>
2021-04-21 18:45:24 -0500

    Output added to Ballpath
    Added an enum based output program to the ballpath class

--- src/main/java/org/wildstang/year2021/robot/CANConstants.java
+++ src/main/java/org/wildstang/year2021/robot/CANConstants.java
@@ -12,6 +12,7 @@ public final class CANConstants {
     public static final int DRIVE_LEFT = 2;
     public static final int DRIVE_RIGHT = 3;
     public static final int INTAKE_MOTOR = 4;
+    public static final int OUTPUT_MOTOR = 4;
 
     public static final int EXAMPLE_CONTROLLER = 1;
     public static final int[] EXAMPLE_PAIRED_CONTROLLERS = {2,3};

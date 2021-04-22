cda2ebf2d88ce2628d579402f2dbcefcd686e375
WhiteHoodHacker <TheWhiteHoodHacker@gmail.com>
2021-04-21 19:05:31 -0500

    Removed unnecessary imports

--- src/main/java/org/wildstang/year2021/subsystems/intake/Intake.java
+++ src/main/java/org/wildstang/year2021/subsystems/intake/Intake.java
@@ -3,10 +3,7 @@ package org.wildstang.year2021.subsystems.intake;
 import org.wildstang.year2021.robot.CANConstants;
 import org.wildstang.year2021.robot.WSInputs;
 
-import java.util.Map;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import org.wildstang.framework.core.Core;

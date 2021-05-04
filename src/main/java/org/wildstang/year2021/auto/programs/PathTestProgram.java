c15f50e7d5590937d2d39eb87db22c62a7946dbd
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2021-05-04 16:32:38 -0500

    Create PathTestProgram.java
    To test PathStep.

---
+++ src/main/java/org/wildstang/year2021/auto/programs/PathTestProgram.java
@@ -0,0 +1,26 @@
+package org.wildstang.year2021.auto.programs;
+
+import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.year2021.auto.steps.DelayStep;
+import org.wildstang.year2021.auto.steps.PathStep;
+/*
+ For testing PathStep.
+ - set RobotWidth and MaxSpeed to reasonable values with correct units in PathStep before running. SpeedConstant may be too low right now.
+ 
+ Hopefully makes the robot move through some points, ending about two units right, two units down relitive to its starting point. Ideally, it ends with about the same orientation.
+ */
+public class ExampleAutoProgram extends AutoProgram {
+
+    @Override
+    protected void defineSteps() {
+        // define a series of steps
+        addStep(new DelayStep(5));
+        addStep(new PathStep([0,2,2,0,-2],[0,0,2,0,-2],[0,0,999,-1,0]);
+    }
+
+    @Override
+    public String toString() {
+        return "PathTestProgram";
+    }
+
+}

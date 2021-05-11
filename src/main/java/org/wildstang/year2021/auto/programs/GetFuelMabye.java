f7cc22ecd5b23180ed4fd5aa98757e17e118057c
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2021-05-10 20:17:33 -0500

    Update GetFuelMabye.java
    I estimate my estimate was way off.

--- src/main/java/org/wildstang/year2021/auto/programs/GetFuelMabye.java
+++ src/main/java/org/wildstang/year2021/auto/programs/GetFuelMabye.java
@@ -14,10 +14,10 @@ public class GetFuelMabye extends AutoProgram {
     protected void defineSteps() {
 
         // define a series of steps
-        double[] Xs ={0,1,1};
-        double[] Ys = {0,0,3};
-        double[] DyDxs = {0,PI/2,PI/2};
-        double[] Speeds = {1,1,1};
+        double[] Xs ={0,3,3,3};
+        double[] Ys = {0,0,7,7};
+        double[] DyDxs = {0,PI/2,PI/2,0.75*PI};
+        double[] Speeds = {1,1,1,0.25};
         addStep(new DelayStep(5));
         PathStep path = new PathStep(Xs,Ys,DyDxs,Speeds);
         //path.PathStep(Xs,Ys,DyDxs,Speeds);
@@ -25,11 +25,10 @@ public class GetFuelMabye extends AutoProgram {
         //deploy high fuel here
         addStep(new DelayStep(1));
         //un-deploy highfuel
-        double[] Xs2 ={1,1,1,0};
-        double[] Ys2 = {3,0,-3,0};
-        double[] DyDxs2 = {0,1.5*PI,1.5*PI,0};
-        double[] Speeds2 = {1,-1,1,-1};
-        addStep(new DelayStep(5));
+        double[] Xs2 ={3,3,3,3,0};
+        double[] Ys2 = {7,0,-7,-7,0};
+        double[] DyDxs2 = {0.75*PI,1.5*PI,1.5*PI,1.25*PI,0};
+        double[] Speeds2 = {1,-1,1,0.25,-1};
         PathStep path2 = new PathStep(Xs2,Ys2,DyDxs2,Speeds2);
         addStep(path2);
 

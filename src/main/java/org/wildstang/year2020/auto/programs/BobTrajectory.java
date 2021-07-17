9149124fc8c2318c03eebfc489c7ed0b52a13f23
Stuart McCrorie <42500981+smccrorie@users.noreply.github.com>
2021-07-17 16:03:36 -0500

    Now uses Bobtrajectory instead of the Vannaka motion profile generator
    Should (theoretically) work with no issues - format of the csv files was the same

---
+++ src/main/java/org/wildstang/year2020/auto/programs/BobTrajectory.java
@@ -0,0 +1,9 @@
+package org.wildstang.year2020.auto.programs;
+
+import com.team319.ui.BobTrajectoryApp;
+
+public class BobTrajectory {
+    public static void main(String... args) {
+        new BobTrajectoryApp();
+    }
+}
